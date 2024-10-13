1dd86a8ef8b52d32fa5837b57e213bf9309ffc37
hlagoda <lagodahubert@gmail.com>
2024-10-13 11:37:56 +0200

    proba1

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import string
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def sortowanie(macierz):
+    posortowane_listy = []
+    
+    for lista in macierz:
+        lista_bez_zer = [element for element in lista if element[1] != 0]
+        
+        n = len(lista_bez_zer)
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if lista_bez_zer[j][1] < lista_bez_zer[j+1][1]:
+                    lista_bez_zer[j], lista_bez_zer[j+1] = lista_bez_zer[j+1], lista_bez_zer[j]
+        
+        posortowane_listy.append(lista_bez_zer)
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+    reduced_lists = [[sublist[0] for sublist in outer_list] for outer_list in posortowane_listy]
+    for i in reduced_lists:
+        print(i) 
 
-x=1
+def zadanie():
+
+    liczba_zdan = int(input("Podaj liczbe zdan \n")).strip()
+    zdania = []
+    for i in range(0, liczba_zdan):
+        zdanie_input = input(f"Podaj {i+1} zdanie: \n").strip()
+
+        zdanie_bez_interpunkcji = zdanie_input.translate(str.maketrans('', '', string.punctuation))
+        zdania.append(zdanie_bez_interpunkcji)
+
+    liczba_slow = int(input("Podaj liczbę słów: \n")).strip()
+    pusta_macierz = [[[i, 0] for i in range(liczba_zdan)] for _ in range(liczba_slow)]
+    for i in range(0, liczba_slow): 
+        slowo = input("Podaj słowo: \n").strip()
+        for jedno_zdanie in range(0, len(zdania)):
+            rozbite_zdanie = zdania[jedno_zdanie].strip().lower().split(" ")
+            for j in range(0, len(rozbite_zdanie)):
+                if rozbite_zdanie[j] == slowo:
+                    pusta_macierz[i][jedno_zdanie][1] += 1
+
+    sortowanie(pusta_macierz)
+
+zadanie()
